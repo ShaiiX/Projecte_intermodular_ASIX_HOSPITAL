4ff96ed17c9dc2317c0e9733a1133de0ef68029e
ShaiiX <shailamgm07@gmail.com>
2026-04-20 09:39:34 +0200

    Rename masked views for consistency and clarity

--- Esquema-seguretat/datamasking.sql
+++ Esquema-seguretat/datamasking.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW pacient_masked AS    -- dades protegides pel pacient
+CREATE OR REPLACE VIEW pacient_mask AS    -- dades protegides pel pacient
 SELECT id_pacient, nom, cognoms,
     CONCAT('***', RIGHT(dni, 3)) AS dni,    -- dni amb els ultims 3 caracters
     '***' AS telefon,   -- telefon no es mostra
@@ -6,20 +6,20 @@ SELECT id_pacient, nom, cognoms,
 FROM pacient;
 
 -- expedient mèdic
-CREATE OR REPLACE VIEW expedient_masked AS
+CREATE OR REPLACE VIEW expedient_mask AS
 SELECT id_expedient, id_pacient,
     '*** DADES PROTEGIDES ***' AS historial,    -- ocultar dades sensibles
     data_actualitzacio
 FROM expedient;
 
 -- vista de vistes amb diagnòstic ocult
-CREATE OR REPLACE VIEW visita_masked AS
+CREATE OR REPLACE VIEW visita_mask AS
 SELECT id_visita, id_pacient, data_visita,
     '***' AS diagnostic, id_metge
 FROM visita;
 
 -- vista receptes ocultes
-CREATE OR REPLACE VIEW recepta_masked AS
+CREATE OR REPLACE VIEW recepta_mask AS
 SELECT id_recepta, id_pacient,
     '***' AS descripcio, data_recepta
 FROM recepta;
