0413797e87cdaf452fc3f7f18e7da05cb9a2d795
ShaiiX <shailamgm07@gmail.com>
2026-04-21 16:52:42 +0200

    Update vari_role permissions: remove access to cantina schema and retain access to estructura schema

--- Esquema-seguretat/rols-permisos/permisos-vari.sql
+++ Esquema-seguretat/rols-permisos/permisos-vari.sql
@@ -1,9 +1,6 @@
 GRANT USAGE ON SCHEMA pacient TO vari_role;
 GRANT USAGE ON SCHEMA estructura TO vari_role;
-GRANT USAGE ON SCHEMA cantina TO vari_role;
 
 GRANT SELECT ON pacient.PACIENT TO vari_role;
 GRANT SELECT ON pacient.INGRES TO vari_role;
 GRANT SELECT ON estructura.HABITACIO TO vari_role;
-GRANT ALL ON cantina.EMPRESA_EXTERNA TO vari_role;
-GRANT ALL ON cantina.FACTURACIO_CANTINA TO vari_role;
